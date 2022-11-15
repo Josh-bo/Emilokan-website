620cc7684f8ce937d4b13071527ad6e442a7fdc2
Bamidele joshua olayinka <Olajoshua775@gmail.com>
2022-11-15 10:06:33 +0100

    I add everything

---
+++ index.js
@@ -0,0 +1,3 @@
+function buy(){
+    alert("Dear customer, we have added your order to your chart, and we have sent a message to your email account, We will get back to you as soon as we can.  Thanks for your patronage...")
+}
